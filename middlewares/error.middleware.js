bbc2eb058e88ed31b947a8980d42a0074c7f269e
Youngben1 <benjaminrenny@gmail.com>
2025-03-28 15:25:01 +0100

    bug fixes and typos

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,4 +1,4 @@
-const errorMiddleware = (err, req, res, next) ={
+const errorMiddleware = (err, req, res, next) =>{
     try {
         let error = { ...err };
 
@@ -6,20 +6,31 @@ const errorMiddleware = (err, req, res, next) ={
 
         console.error(err);
 
+         // Handle CastError (e.g., invalid MongoDB ObjectId)
         if(err.name === 'CastError') {
             const message = 'Resource not found';
             error = new Error(message);
             error.statusCode = 404;
         }
 
+        // Handle MongoDB duplicate key error
         if(err.name === 11000) {
             const message = 'Duplicate field value entered';
             error = new Error(message);
             error.statusCode = 400;
         }
 
+        // Handle Mongoose validation errors
+        if(err.name === "ValidationError") {
+            const message = Object.values(err.errors).map((value) =>value.message);
+            error = new Error(message.join(', '));
+            error.statusCode = 400;
+        }
 
+        res.statusCode(error.statusCode || 500).json({success: false, error: error.message || 'Server Error'});
     } catch (error) {
         next(error);
     }
 }
+
+export default errorMiddleware;
